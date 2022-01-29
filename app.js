6e9381b3ce6ba4c339058f216efb4f0c8035bafe
Ana Luzardo <anitaebratt@gmail.com>
2022-01-28 21:53:19 -0300

    creando registro

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
 });
 
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
 app.listen(3000, ()=>{
     console.log('Servidor funcionando');
 });
